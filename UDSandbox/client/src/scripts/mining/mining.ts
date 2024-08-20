ff622c07c90c2ea83ec163742db0d3baff6693f8
unknown3211 <100625853+unknown3211@users.noreply.github.com>
2024-08-20 00:42:45 -0400

    Mining Config Created

--- UDSandbox/client/src/scripts/mining/mining.ts
+++ UDSandbox/client/src/scripts/mining/mining.ts
@@ -3,13 +3,14 @@ import { cube, cubeBody, world } from '../../dev/map';
 import { scene, currentUserId } from '../../main';
 import { inventory } from '../inventory/inventory';
 import Sound from '../../assets/sounds/boom1.wav';
+import { CopperConfig } from './miningconfig';
 
 export function MineCopper() {
     if(currentUserId) {
-        Progressbar('Mining', 'Mining Copper', 2000);
-        inventory.addItem(4, 1, currentUserId);
-        Delay(2000).then(() => {
-            Notify('Job Center', "Copper Mined", 2000, 'success');
+        Progressbar(CopperConfig.progresstitle, CopperConfig.progressmessage, CopperConfig.progressduration);
+        inventory.addItem(CopperConfig.item, CopperConfig.amount, currentUserId);
+        Delay(CopperConfig.progressduration).then(() => {
+            Notify(CopperConfig.notifyTitle, CopperConfig.notifyMessage, CopperConfig.notifyDuration, 'success');
             console.log(currentUserId);
             PlaySound(Sound, 0.5, 2000);
             scene.remove(cube);
